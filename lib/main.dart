c6eb8927f40ab47ca3fb63c6f0a15f1c0137a851
Erick <erick.heredia@gmail.com>
2020-04-27 18:17:18 -0400

    Modificando orientación de pantalla
    Esta modificación permite fijar la dirección de la pantalla (portrait up).

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:device_preview/device_preview.dart';
 import 'package:epidemiologia_latam/app.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as services;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await services.SystemChrome.setPreferredOrientations(
+    [
+      services.DeviceOrientation.portraitUp,
+    ],
+  );
+
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
