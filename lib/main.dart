16babb4f800b3f2aff76cc56845903d79929997b
Erick Heredia <erick.heredia@gmail.com>
2020-05-04 11:19:05 -0400

    Cambiando a import locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:epidemiologia_latam/app.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as services;
 
+import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await services.SystemChrome.setPreferredOrientations(
