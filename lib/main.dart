892cbeb5ada1b1c6607f2e07044079d55f9d8368
Erick Heredia <erick.heredia@gmail.com>
2020-05-04 20:07:51 -0400

    Inicializando config reader en main.
    Inicializando config reader en main usando variables remotas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:device_preview/device_preview.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as services;
 
 import 'app.dart';
 import 'app/locator.dart';
+import 'utils/config_reader.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // * Supporting PortraitUp Orientation
   await services.SystemChrome.setPreferredOrientations(
     [
       services.DeviceOrientation.portraitUp,
     ],
   );
 
+  // * Getting data from CI/CD
+  await ConfigReader.initialize();
+
+  // * Getting Inversion of control (Dependency Locator)
   configure();
 
   runApp(
