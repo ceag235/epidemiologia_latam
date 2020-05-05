e15bcdcc1641420b9f01bc1b256a4ceb0f55ddf1
Erick Heredia <erick.heredia@gmail.com>
2020-05-05 16:14:19 -0400

    Agregando archivo de rutas de manera local

--- lib/ui/views/splash/splash_viewmodel.dart
+++ lib/ui/views/splash/splash_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/locator.dart';
+import '../../../app/routes.gr.dart';
 
 class SplashViewModel extends BaseViewModel {
   //  Services
