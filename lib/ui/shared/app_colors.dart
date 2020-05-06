552b51bebaee7a742b449a7cf9d657b40be4198f
Erick Heredia <erick.heredia@gmail.com>
2020-05-05 23:01:14 -0400

    Modificando parametro de opacidad
    El parametro de opacidad va de 0 a 1.

--- lib/ui/shared/app_colors.dart
+++ lib/ui/shared/app_colors.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 // * Darker colors
-const Color darkPurple = const Color.fromRGBO(108, 99, 255, 255); //  #6C63FF
-const Color darkSoftBlue = const Color.fromRGBO(85, 147, 241, 255); //  #5593F1
-const Color darkBlue = const Color.fromRGBO(0, 62, 155, 255); //  #003E9B
+const Color darkPurple = const Color.fromRGBO(108, 99, 255, 1.0); //  #6C63FF
+const Color darkSoftBlue = const Color.fromRGBO(85, 147, 241, 1.0); //  #5593F1
+const Color darkBlue = const Color.fromRGBO(0, 62, 155, 1.0); //  #003E9B
 
 // * Lighter Colors
-const Color lightGreen = const Color.fromRGBO(144, 188, 49, 255); //  #90BC31
-const Color lightBlue = const Color.fromRGBO(174, 221, 227, 255); //  #AEDDE3
-const Color lightPink = const Color.fromRGBO(247, 189, 189, 255); //  #F7BDBD
-const Color lightRed = const Color.fromRGBO(212, 96, 107, 255); //  #D4606B
-const Color lightOrange = const Color.fromRGBO(252, 93, 93, 255); //  #FC5D5D
+const Color lightGreen = const Color.fromRGBO(144, 188, 49, 1.0); //  #90BC31
+const Color lightBlue = const Color.fromRGBO(174, 221, 227, 1.0); //  #AEDDE3
+const Color lightPink = const Color.fromRGBO(247, 189, 189, 1.0); //  #F7BDBD
+const Color lightRed = const Color.fromRGBO(212, 96, 107, 1.0); //  #D4606B
+const Color lightOrange = const Color.fromRGBO(252, 93, 93, 1.0); //  #FC5D5D
 
 // * Monochromatic Colors
-const Color whiteMonoLetter = const Color.fromRGBO(255, 255, 255, 255); //  #FFF
-const Color lightMonoGrey = const Color.fromRGBO(223, 223, 223, 255); // #DFDFDF
-const Color monoGrey = const Color.fromRGBO(167, 167, 167, 255); //  #A7A7A7
+const Color whiteMonoLetter = const Color.fromRGBO(255, 255, 255, 1.0); //  #FFF
+const Color lightMonoGrey = const Color.fromRGBO(223, 223, 223, 1.0); // #DFDFDF
+const Color monoGrey = const Color.fromRGBO(167, 167, 167, 1.0); //  #A7A7A7
 const Color mediumMonoGrey =
-    const Color.fromRGBO(123, 121, 110, 255); //  #7B796E
-const Color darkMonoGrey = const Color.fromRGBO(112, 112, 112, 255); //  #707070
-const Color blackMonoLetter = const Color.fromRGBO(45, 43, 42, 255); //  #2D2B2A
+    const Color.fromRGBO(123, 121, 110, 1.0); //  #7B796E
+const Color darkMonoGrey = const Color.fromRGBO(112, 112, 112, 1.0); //  #707070
+const Color blackMonoLetter = const Color.fromRGBO(45, 43, 42, 1.0); //  #2D2B2A
